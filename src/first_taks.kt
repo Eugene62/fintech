5753663849ea33ebaf3a43c11c8c0be9e18351e6
Eugene <mirohin.e@gmail.com>
2019-09-26 21:14:45 +0300

    сделал ввод имени и чисел, пока без проверки на пустые строки

---
+++ src/first_taks.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>) {
+    println("Enter your name:")
+    val name = readLine()
+
+    println("Enter first number:")
+    val firstNumber = readLine()
+
+    println("Enter second number:")
+    val secondNumber = readLine()
+
+    println("$name gets $firstNumber through $secondNumber ")
+}
