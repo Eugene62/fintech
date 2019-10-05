8d3b963040e6005a008db06998b19579ce6c327e
Eugene <mirohin.e@gmail.com>
2019-10-05 10:59:00 +0300

    добавил фалй с классами и создал объекты в мейне

---
+++ src/animals.kt
@@ -0,0 +1,16 @@
+package rutinkoff
+
+class Dog(name:String, age: UInt) {
+}
+
+class Cat( name:String, age:UInt ) {
+}
+
+class Parrot( name:String, age:UInt ) {
+}
+
+class Fish( name:String, age:UInt ) {
+}
+
+class Snake( name:String, age:UInt ) {
+}
---
+++ src/main.kt
@@ -0,0 +1,9 @@
+package rutinkoff
+
+fun main( args: Array<String> ) {
+    val dog = Dog("Goofy", 23u )
+    val cat = Cat("Tom", 7u )
+    val parrot = Parrot( "Iago", 12u )
+    val snake = Snake( "Kaa", 48u )
+    val fish = Fish( "Nemo", 2u )
+}
