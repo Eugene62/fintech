b670989ae77ad95588de1b260d4ce4be3ec2a025
Eugene <mirohin.e@gmail.com>
2019-11-04 11:32:35 +0300

    вывел смешариков

---
+++ src/smehariki.kt
@@ -0,0 +1,12 @@
+package ru.tinkoff
+
+fun main() {
+
+    listOf<String>("Крош", "Ёжик", "Нюша", "Бараш").forEach{
+        println(it)
+    }
+
+    setOf<String>("Крош", "Ёжик", "Нюша", "Бараш").forEach{
+        println(it)
+    }
+}
