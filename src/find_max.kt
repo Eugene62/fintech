e048fd5886e6f6c1bbe494ba65b3e9df42ccf988
Eugene <mirohin.e@gmail.com>
2019-11-06 22:53:46 +0300

    имя максимальой длины

---
+++ src/find_max.kt
@@ -0,0 +1,14 @@
+package ru.tinkoff.lesson_12.find_max
+
+fun main() {
+    val myGoodNightTeam = listOf("Хрюша", "Степаша", "Филя", "Гуля")
+    
+    val maxLength = myGoodNightTeam
+        .asSequence()
+        .map { it.length }
+        .max()
+
+    val nameWithMaxSize = myGoodNightTeam.find { it.length == maxLength }
+
+    println(nameWithMaxSize)
+}
