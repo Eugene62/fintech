2cc3bdd22b828d1e54ebc327d1361a01ca51cbba
Eugene <mirohin.e@gmail.com>
2019-11-06 23:17:27 +0300

    сделал приветливые имена, длиной не больше 4

--- src/asSequence.kt
+++ src/asSequence.kt
@@ -1,9 +1,15 @@
 package ru.tinkoff.lesson_12
 
 fun main() {
-     var myMutableList = mutableListOf("Хрюша", "Степаша", "Филя", "Гуля")
+    var myGoodNightTeam = mutableListOf("Хрюша", "Степаша", "Филя", "Гуля")
 
-    myMutableList.asSequence().filter { it.length <= 4 }
-    
+    val myNeighborlyGoodNightTeam = myGoodNightTeam
+                                                                .asSequence()
+                                                                .filter { it.length <= 4 }
+                                                                .map { it.let { "Привет, $it" } }
+
+    for (elem in myNeighborlyGoodNightTeam)
+        println(elem)
+
 }
 
--- src/find_max.kt
+++ src/find_max.kt
@@ -2,7 +2,7 @@ package ru.tinkoff.lesson_12.find_max
 
 fun main() {
     val myGoodNightTeam = listOf("Хрюша", "Степаша", "Филя", "Гуля")
-    
+
     val maxLength = myGoodNightTeam
         .asSequence()
         .map { it.length }
