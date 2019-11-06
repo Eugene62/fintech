c21a05879e789ee9c6decdf9c103bf6154aa72d5
Eugene <mirohin.e@gmail.com>
2019-11-06 21:00:48 +0300

    сделал задание с map

--- src/asSequence.kt
+++ src/asSequence.kt
@@ -1,9 +1,9 @@
 package ru.tinkoff.lesson_12
 
 fun main() {
-    listOf("Хрюша", "Степаша", "Филя", "Гуля")
-        .asSequence()
-        .filter { it.length <= 4 }
-        .forEach { println("Привет, ${it}") }
+     var myMutableList = mutableListOf("Хрюша", "Степаша", "Филя", "Гуля")
+
+    myMutableList.asSequence().filter { it.length <= 4 }
+    
 }
 
---
+++ src/map.kt
@@ -0,0 +1,8 @@
+package ru.tinkoff.lesson_12.map
+
+fun main() {
+    val myFrendlyList = listOf("Хрюша", "Степаша", "Филя", "Гуля").map { it.let { "Привет, $it" } }
+
+    for (i in myFrendlyList)
+        println(i)
+}
