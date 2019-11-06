4a02938febb59d25b6a9cd0e1feff49fd4f68b4f
Eugene <mirohin.e@gmail.com>
2019-11-06 23:21:13 +0300

    поправил табуляци.

--- src/asSequence.kt
+++ src/asSequence.kt
@@ -4,9 +4,9 @@ fun main() {
     var myGoodNightTeam = mutableListOf("Хрюша", "Степаша", "Филя", "Гуля")
 
     val myNeighborlyGoodNightTeam = myGoodNightTeam
-                                                        .asSequence()
-                                                        .filter { it.length <= 4 }
-                                                        .map { it.let { "Привет, $it" } }
+        .asSequence()
+        .filter { it.length <= 4 }
+        .map { it.let { "Привет, $it" } }
 
     for (elem in myNeighborlyGoodNightTeam)
         println(elem)
