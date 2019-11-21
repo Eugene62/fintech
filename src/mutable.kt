8f6aec5e9299528cda0d61e80c4cd232398e626c
mirohin.e <mirohin.e.i@tor.rsreu.ru>
2019-11-21 10:35:50 +0300

    убрал плохую инициализацию

--- src/mutable.kt
+++ src/mutable.kt
@@ -1,21 +1,21 @@
-package ru.tinkoff
+package ru.tinkoff.lesson_8
 
 fun main() {
     val myArrayList: List<String> = listOf("Крош", "Ёжик", "Нюша", "Бараш")
 
-    var myMutarrayList: ArrayList<String> = mutableListOf<String>() as ArrayList<String>
+    var myMutArrayList = arrayListOf<String>()
 
     for (elem in myArrayList)
-        myMutarrayList.add(elem + " array")
+        myMutArrayList.add(elem + " array")
 
     println("Переписал")
 
-    for (elem in myMutarrayList)
+    for (elem in myMutArrayList)
         println(elem)
 
     val myHashSet: HashSet<String> = hashSetOf("Крош", "Ёжик", "Нюша", "Бараш")
 
-    var myMutHashSet: HashSet<String> = mutableSetOf<String>() as HashSet<String>
+    var myMutHashSet: HashSet<String> = hashSetOf<String>()
 
     for (elem in myHashSet)
         myMutHashSet.add(elem + " hashSet")
@@ -25,4 +25,3 @@ fun main() {
     for (elem in myMutHashSet)
         println(elem)
 }
-
